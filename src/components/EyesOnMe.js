164bf8e678729b81b04356ddeab53ffc2a56f99a
var787 <hegde.rakshan@gmail.com>
2021-01-13 00:15:36 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+export default class EyesOnMe extends React.Component {
+    eventFocus = () => console.log('Good!')
+    eventBlur = () => console.log('Hey! Eyes on me!')
+    render(){
+        return <div>
+            <button 
+            onFocus={this.eventFocus} onBlur={this.eventBlur}>
+            </button>
+
+        </div>
+}
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React from "react";
+
+export default class Keypad extends React.Component {
+    
+    eventPassword = () => console.log('Entering password...')
+    
+    render(){
+        return <div><input onKeyUp={this.eventPassword} type="password"/>
+        </div>
+    }
+}
